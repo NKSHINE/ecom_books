243979a2d60fa7a8059ed476bb3faed5677af7d3
Dhuja Shaheem <shaheemdhuja@gmail.com>
2025-06-24 22:43:19 +0530

    commit10

--- client/src/components/AddBookSection.jsx
+++ client/src/components/AddBookSection.jsx
@@ -1,6 +1,7 @@
 // AddBookSection.jsx
 import React, { useState } from "react";
 import axios from "axios";
+
 function AddBookSection({ setFetchTrigger }) {
   const [form, setForm] = useState({
     title: "", author: "", genre: "", description: "",
@@ -37,39 +38,47 @@ function AddBookSection({ setFetchTrigger }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="row g-3">
-      {Object.entries({ title: "", author: "", genre: "", description: "", price: "", stock: "", language: "", publisher: "", image: "" }).map(([field], i) => (
-        <div className="col-md-6" key={i}>
-          <label className="form-label">{field}</label>
-          <input
-            type={["price", "stock"].includes(field) ? "number" : "text"}
-            name={field}
-            className="form-control"
-            value={form[field]}
-            onChange={handleChange}
-          />
-        </div>
-      ))}
-      <div className="col-md-6">
-        <label className="form-label">Tags</label>
-        <input type="text" name="tags" className="form-control" value={form.tags} onChange={handleChange} />
+    <div className="container my-4">
+      <div className="p-4 shadow rounded bg-white w-100">
+        <h4 className="mb-4">Add a New Book</h4>
+        <form onSubmit={handleSubmit} className="row g-3">
+          {Object.entries({ title: "", author: "", genre: "", description: "", price: "", stock: "", language: "", publisher: "", image: "" }).map(([field], i) => (
+            <div className="col-md-6" key={i}>
+              <label className="form-label text-capitalize">{field}</label>
+              <input
+                type={["price", "stock"].includes(field) ? "number" : "text"}
+                name={field}
+                className="form-control"
+                value={form[field]}
+                onChange={handleChange}
+              />
+            </div>
+          ))}
+          <div className="col-md-6">
+            <label className="form-label">Tags</label>
+            <input type="text" name="tags" className="form-control" value={form.tags} onChange={handleChange} />
+          </div>
+          <div className="col-md-3 d-flex align-items-end">
+            <div>
+              <label className="form-label d-block">Featured</label>
+              <input type="checkbox" name="is_featured" checked={form.is_featured} onChange={handleChange} />
+            </div>
+          </div>
+          <div className="col-md-3">
+            <label className="form-label">Discount %</label>
+            <input type="number" name="offer.discount_percent" className="form-control" value={form.offer.discount_percent} onChange={handleChange} />
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">Valid Till</label>
+            <input type="date" name="offer.valid_till" className="form-control" value={form.offer.valid_till} onChange={handleChange} />
+          </div>
+          <div className="col-12">
+            <button className="btn btn-primary w-100">Add Book</button>
+          </div>
+        </form>
       </div>
-      <div className="col-md-3">
-        <label className="form-label">Featured</label><br />
-        <input type="checkbox" name="is_featured" checked={form.is_featured} onChange={handleChange} />
-      </div>
-      <div className="col-md-3">
-        <label className="form-label">Discount %</label>
-        <input type="number" name="offer.discount_percent" className="form-control" value={form.offer.discount_percent} onChange={handleChange} />
-      </div>
-      <div className="col-md-6">
-        <label className="form-label">Valid Till</label>
-        <input type="date" name="offer.valid_till" className="form-control" value={form.offer.valid_till} onChange={handleChange} />
-      </div>
-      <div className="col-12">
-        <button className="btn btn-primary">Add Book</button>
-      </div>
-    </form>
+    </div>
   );
 }
+
 export default AddBookSection;
